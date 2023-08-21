882bd87ed95fa7a7e70558bdc7bece02a310fa87
Andres <afcajamarcar@unal.edu.co>
2023-08-21 14:31:37 -0400

    add mising packages and one unitary test

--- src/app/tests/card.spec.tsx
+++ src/app/tests/card.spec.tsx
@@ -1,3 +1,30 @@
-// TODO create tests here
-const myTest = () => {}
-export default myTest
+import { render, screen } from '@testing-library/react'
+import Card from "../components/card"
+
+const CardTest = () => {
+  const setup = (propsOverride: any = {}) => {
+    const props = {
+      composedInfo: 'ALDO Centre Eaton/MIRIRA',
+      inventory: '9',
+      ...propsOverride,
+    }
+
+    const renderWrapper = render(<Card {...props} />)
+
+    return {
+      renderWrapper,
+      props,
+    }
+  }
+
+  describe('<Card> component', () => {
+    it('should display the name of the store', () => {
+      const { props: { composedInfo } } = setup()
+      const storeName = composedInfo.split('/')[0]
+      const storeNameHeader = screen.getByText(storeName)
+      expect(storeNameHeader).toBeDefined()
+    })
+  })
+}
+
+export default CardTest
