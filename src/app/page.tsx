ceffa56d693ff7617505b7213270041c14fa1b9b
Andres <afcajamarcar@unal.edu.co>
2023-08-21 11:24:45 -0400

    add connection to inventory's tap, add Map as base state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,21 @@
+'use client'
+
+import { useState, useEffect } from "react"
+
 export default function Home() {
+  const [data, setData] = useState<Map<String, Number>>(new Map())
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080/')
+    console.log('Connection stablished');
+
+    ws.onmessage = (event: any) => {
+      const { store, model, inventory } = JSON.parse(event.data)
+      setData(prevState => new Map(prevState.set(`${store}/${model}`, inventory)))
+    }
+  }, [])
+
+  console.log(data);
+
   return (
     <main>
     </main>
