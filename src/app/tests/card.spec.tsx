c2353b0d5ce11f2788dd769c64f0282477726063
Andres <afcajamarcar@unal.edu.co>
2023-08-21 14:51:54 -0400

    solve config problems in tests files

--- src/app/tests/card.spec.tsx
+++ src/app/tests/card.spec.tsx
@@ -1,30 +1,26 @@
 import { render, screen } from '@testing-library/react'
 import Card from "../components/card"
 
-const CardTest = () => {
-  const setup = (propsOverride: any = {}) => {
-    const props = {
-      composedInfo: 'ALDO Centre Eaton/MIRIRA',
-      inventory: '9',
-      ...propsOverride,
-    }
-
-    const renderWrapper = render(<Card {...props} />)
-
-    return {
-      renderWrapper,
-      props,
-    }
+const setup = (propsOverride: any = {}) => {
+  const props = {
+    composedInfo: 'ALDO Centre Eaton/MIRIRA',
+    inventory: '9',
+    ...propsOverride,
   }
 
-  describe('<Card> component', () => {
-    it('should display the name of the store', () => {
-      const { props: { composedInfo } } = setup()
-      const storeName = composedInfo.split('/')[0]
-      const storeNameHeader = screen.getByText(storeName)
-      expect(storeNameHeader).toBeDefined()
-    })
-  })
+  const renderWrapper = render(<Card {...props} />)
+
+  return {
+    renderWrapper,
+    props,
+  }
 }
 
-export default CardTest
+describe('<Card> component', () => {
+  it('should display the name of the store', () => {
+    const { props: { composedInfo } } = setup()
+    const storeName = composedInfo.split('/')[0]
+    const storeNameHeader = screen.getByText(storeName)
+    expect(storeNameHeader).toBeDefined()
+  })
+})
