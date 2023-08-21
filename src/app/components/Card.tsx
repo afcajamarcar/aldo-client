7dab7d6048aae19ec4a2aa39bae3dd6813f1c068
Andres <afcajamarcar@unal.edu.co>
2023-08-21 18:27:00 -0400

    fix identation and styles

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,6 +1,6 @@
 const Card = (
   { composedInfo, inventory }:
-  { composedInfo: string, inventory: string | undefined }): JSX.Element => {
+    { composedInfo: string, inventory: string | undefined }): JSX.Element => {
   const nameAndProduct = composedInfo.split('/')
 
   return (
@@ -8,7 +8,12 @@ const Card = (
       className="flex flex-col justify-center items-center box-border h-148 w-248 p-4 m-4 border-4"
       key={composedInfo}
     >
-      <h1 className="font-bold text-lg max-w-xs truncate ...">{nameAndProduct[0]}</h1>
+      <h1
+        aria-label={nameAndProduct[0]}
+        title={nameAndProduct[0]}
+        className="font-bold text-lg max-w-xs truncate ...">
+          {nameAndProduct[0]}
+      </h1>
       <h2>{nameAndProduct[1]}</h2>
       <p>{inventory}</p>
     </section>
