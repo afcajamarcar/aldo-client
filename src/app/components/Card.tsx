4baee99949190b71269b0af4bef2ccbf4e1f9a7b
Andres <afcajamarcar@unal.edu.co>
2023-08-21 23:24:31 -0400

    refactor card component to be used as status card and notification card, add notifications array as state and refactor

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,23 +1,26 @@
 const Card = (
-  { composedInfo, inventory }:
-    { composedInfo: string, inventory: string | undefined }): JSX.Element => {
-  const nameAndProduct = composedInfo.split('/')
-
-  return (
-    <section
-      className="flex flex-col justify-center items-center shadow-lg h-148 w-248 p-4 m-4 border"
-      key={composedInfo}
+  { store, model, inventory }:
+    {
+      store: string,
+      model: string,
+      inventory: string | undefined,
+      notification?: string,
+    }
+): JSX.Element => (
+  <section
+    className="flex flex-col justify-center items-center shadow-lg h-148 w-248 p-4 m-4 border"
+    key={`${store}/${model}`}
+  >
+    <h1
+      aria-label={store}
+      title={store}
+      className="font-bold text-lg max-w-xs truncate ..."
     >
-      <h1
-        aria-label={nameAndProduct[0]}
-        title={nameAndProduct[0]}
-        className="font-bold text-lg max-w-xs truncate ...">
-          {nameAndProduct[0]}
-      </h1>
-      <h2 className="text-lg">{nameAndProduct[1]}</h2>
-      <p className="font-bold text-2xl transition-opacity">{inventory}</p>
-    </section>
-  )
-}
+      {store}
+    </h1>
+    <h2 className="text-lg">{model}</h2>
+    <p className="font-bold text-2xl transition-opacity">{inventory}</p>
+  </section>
+)
 
 export default Card
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,56 @@ import { useState, useEffect } from "react"
 import Card from "./components/Card"
 
 export default function Home() {
-  const [data, setData] = useState<Map<string, string>>(new Map())
+  const [storesInventory, setStoresInventory] = useState<Map<string, string>>(new Map())
+  // TODO improve types
+  const [stockNotifications, setStockNotifications] = useState<Array<{ store: string, model: string, inventory: string, notification: string }>>([])
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:8080/')
 
     ws.onmessage = (event: any) => {
       const { store, model, inventory } = JSON.parse(event.data)
-      setData(prevState => new Map(prevState.set(`${store}/${model}`, inventory)))
+      if (Number(inventory) === 0) setStockNotifications(prevState => [...prevState, { store, model, inventory, notification: 'No Stock' }]) //TODO move constant to file
+      if (Number(inventory) <= 10) setStockNotifications(prevState => [...prevState, { store, model, inventory, notification: 'Low Stock' }]) //TODO move constant to file
+      if (Number(inventory) >= 90) setStockNotifications(prevState => [...prevState, { store, model, inventory, notification: 'High Stock' }]) //TODO move constant to file
+      setStoresInventory(prevState => new Map(prevState.set(`${store}/${model}`, inventory)))
     }
   }, [])
 
+  const renderStatusCard = (composedInfo: string): JSX.Element => {
+    const nameAndProduct = composedInfo.split('/')
+    return (
+      <Card
+        store={nameAndProduct[0]}
+        model={nameAndProduct[1]}
+        inventory={storesInventory.get(composedInfo)}
+      />
+    )
+  }
+
   return (
     <main>
-      <section className="max-w-screen-md">
-        <h1 className="text-4xl m-10 text-center">Current Inventory Status</h1>
-        <div className="grid grid-cols-2 gap-2 gap-x-15">
-          {Array.from(data.keys()).map(key => (
-            <Card
-              composedInfo={key}
-              inventory={data.get(key)}
-            />
-          ))}
-        </div>
-      </section>
+      <div className="grid grid-cols-2">
+        <section>
+          <h1 className="text-4xl m-10 text-center">Current Inventory Status</h1>
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-2 gap-x-15">
+            {Array.from(storesInventory.keys()).map(composedInfo => renderStatusCard(composedInfo))}
+          </div>
+        </section>
+        <section>
+          <>
+            <h1 className="text-4xl m-10 text-center">Stock Notifications</h1>
+            {stockNotifications &&
+              stockNotifications.map(({ store, model, inventory, notification }) => (
+                <Card
+                  store={store}
+                  model={model}
+                  inventory={inventory}
+                  notification={notification}
+                />
+              ))}
+          </>
+        </section>
+      </div>
     </main>
   )
 }
